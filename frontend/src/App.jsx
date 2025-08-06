b686f2316e011c881aea07280d2bca3a4f2a9c90
Abdisa.dev <meetabdisa@gmail.com>
2025-08-06 13:54:56 +0300

    updated the  home.jsx and favorites.jsx also the main navbar....

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import NavBar from "./components/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Text />
-      <Text />
-      <Text />
-      <Text />
-    </>
-  );
-}
-
-function Text() {
-  return (
-    <>
-      <p>hello world</p>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
     </>
   );
 }
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import MovieCard from "../components/MovieCard";
+
+import { useState } from "react";
+
+const Home = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const movies = [
+    { id: 1, title: "John Wick", release_date: "2020" },
+    { id: 1, title: "Avengers", release_date: "1999" },
+    { id: 1, title: "The matrix", release_date: "1995" },
+    { id: 1, title: "Terminator", release_date: "2018" },
+  ];
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+    setSearchQuery("--------");
+  };
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="search for movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+
+      <div className="movie-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
